4a5b2ecdad1c2da0153a880f57a45e30e801e749
Rúben Carvalho <ruben@homeserver.server.local>
2025-06-30 22:55:44 +0100

    Initial commit

---
+++ READM.md
@@ -0,0 +1,42 @@
+# Music Downloader
+
+A simple Bash script to download audio from YouTube videos and manage metadata.
+
+## Features
+
+- Downloads audio in MP3 format.
+- Embeds thumbnail and adds metadata.
+- Allows editing of metadata after download.
+- Organizes downloaded music into artist/album directories.
+
+## Requirements
+
+- yt-dlp: A command-line program to download videos from YouTube and other sites.
+- eyeD3: A tool for working with audio files, specifically for editing ID3 tags.
+
+## Usage
+
+1. Clone the repository:
+
+   ```
+   git clone <repository-url>
+   cd music-downloader
+   ```
+
+2. Make the script executable:
+
+   ```
+   chmod +x music.sh
+   ```
+
+3. Run the script with a YouTube URL:
+
+   ```
+   ./music.sh <Youtube URL>
+   ```
+
+4. Follow the prompts to edit metadata if desired.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ music.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Verify the arguments
+if [ -z "$1" ]; then
+    echo "Use like: $0 <Youtube URL>"
+    exit 0
+fi
+
+# Define variables
+URL=$1
+TEMP_DIR="/tmp/music"
+DEST_DIR="/srv/cloud/ruben/Musicas"
+
+# Create (if doesn't exist) the temp directory
+mkdir -p "$TEMP_DIR"
+
+# Download mp3 file
+echo "Downloading music..."
+yt-dlp -x --audio-format mp3 --embed-thumbnail --add-metadata -o "$TEMP_DIR/%(title)s.%(ext)s" "$URL"
+
+# Find the mp3 file
+FILE_PATH=$(find "$TEMP_DIR" -type f -iname "*.mp3" | head -n 1)
+
+# In case of failure
+if [ ! -f "$FILE_PATH" ]; then
+    echo "Error: file not found!"
+    exit 2
+fi
+
+# Show the file
+echo "Downloaded music: $FILE_PATH"
+echo
+
+# Show the metadata
+echo "Actual metadata:"
+eyeD3 "$FILE_PATH"
+echo
+
+# Ask if want to change
+read -p "Do you want to change the metadata? (s/N): " edit
+
+# If want edit
+if [[ "$edit" =~ ^[sS]$ ]]; then
+
+    # Ask the new metadata
+    read -p "Title: " title
+    read -p "Artist: " artist
+    read -p "Album: " album
+
+    # Change the metadata
+    eyeD3 --title "$title" --artist "$artist" --album "$album" "$FILE_PATH"
+    echo "Metadata changed"
+    echo
+
+    # Show the new metadata
+    echo "Final metadata: "
+    eyeD3 "$FILE_PATH"
+
+# If don't want edit
+else
+    artist=$(eyeD3 "$FILE_PATH" | grep "artist:" | head -n 1 | awk -F ': ' '{print $2}' | xargs)
+    album=$(eyeD3 "$FILE_PATH" | grep "album:" | head -n 1 | awk -F ': ' '{print $2}' | xargs)
+fi
+
+# Move to the final directory
+FINAL_DIR="$DEST_DIR/$artist/$album/"
+
+echo "Moving to $FINAL_DIR..."
+
+mkdir -p "$FINAL_DIR"
+mv "$FILE_PATH" "$FINAL_DIR"
+
+# Delete the temp dir
+rm -rf "$TEMP_DIR"
+
+echo "Finished! Music moved to $FINAL_DIR"
